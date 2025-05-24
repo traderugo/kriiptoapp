b29d066cf27060c162f053526ae16552c1610d34
Johnson Ugochukwu <86761845+traderugo@users.noreply.github.com>
2025-05-23 23:06:09 -0700

    your commit message

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between sticky top-0 z-50 shadow">
+    <nav className="bg-white  p-4 flex justify-between sticky top-0 z-50 shadow">
       <div className="font-bold"><Link href="/">
   <Image src="/logo.png" alt="Logo" width={150} height={150} />
 </Link></div>
