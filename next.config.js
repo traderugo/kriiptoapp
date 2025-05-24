199ea5ce76d7d4c266bad38d31cbe72bacef9247
Johnson Ugochukwu <86761845+traderugo@users.noreply.github.com>
2025-05-24 00:28:07 -0700

    your commit message

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // disable in dev
+  
+});
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+});
+
+module.exports = nextConfig;
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
