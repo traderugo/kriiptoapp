9c8054507437779581bbbcf023c09d3ca8e2d751
Johnson Ugochukwu <86761845+traderugo@users.noreply.github.com>
2025-05-25 01:30:25 -0700

    your commit message

--- pages/home.js
+++ pages/home.js
@@ -46,6 +46,7 @@ export default function AdminPage() {
   const adminLinks = [
     { href: "/signals", title: "Signals", description: "List of signals" },
     { href: "/", title: "Calculator", description: "Go to Trade Calculator" },
+    { href: "/", title: "Articles", description: "Free contents on trading and investing" },
     { href: "/add-signals", title: "Add Signals", description: "Admin" },
     { href: "/add-subscribers", title: "Add Subscibers", description: "Admin" },
     { href: "/edit-subscribers", title: "Edit Subscribers", description: "Admin" },
