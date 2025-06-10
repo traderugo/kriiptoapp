6cc89775d5f30c6d936efb6d1034b3c0e6efd559
Johnson Ugochukwu <86761845+traderugo@users.noreply.github.com>
2025-06-10 09:55:04 -0700

    your commit message

--- pages/index.js
+++ pages/index.js
@@ -84,13 +84,13 @@ export default function Home() {
 </section>
 
 </h2>
-        
+<div className="flex items-center justify-center min-h-screen">        
   <Link href="/login">
       <a className="inline-block px-6 py-3 bg-blue-600 text-white font-semibold rounded-xl shadow hover:bg-blue-700 transition">
         Sign in to join
       </a>
     </Link>
-       
+    </div>   
       </div>
        <br />
         
